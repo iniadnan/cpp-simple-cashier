d0787ffc5c4f70b72ccbd4dbb7cb878393b61e72
Adnan Muttaqin <muh.adnan.muttaqin@gmail.com>
2022-01-20 22:29:02 +0700

    Done

--- index.cpp
+++ index.cpp
@@ -8,6 +8,7 @@
 using namespace std;
 
 string items[100];
+string setPassword;
 
 // AMBIL DAFTAR MENU
 void getMenu()
@@ -23,225 +24,273 @@ void getMenu()
     is.close();
 }
 
+// AMBIL DATA PASSWORD
+void getPassword()
+{
+    ifstream is("password.txt");
+    getline(is, setPassword);
+    is.close();
+}
+
 int main()
 {
     getMenu();
-    int menuSize = sizeof(items) / sizeof(items[0]);
+    getPassword();
 
     system("cls");
 
-    // NAMA APLIKASI
-    xc::color(14, 0);
-    cout << "KASIR V.1" << endl
-         << endl;
+    // MASUKAN PASSWORD
+    string inputPassword;
+    cout << "Masukan Password: ";
+    cin >> inputPassword;
 
-    // NAMA DOSEN
-    xc::color(7, 0);
-    cout << "KASIR: ";
-    xc::color(10, 0);
-    cout << "Adnan Muttaqin";
+    if(inputPassword == setPassword) {
+        system("cls");
 
-    cout << endl;
+        int menuSize = sizeof(items) / sizeof(items[0]);
 
-    // TABLE TOP
-    cout << char(218);
-    for (int i = 0; i < 68; i++)
-    {
-        if (i == 6 || i == 44)
+        // NAMA APLIKASI
+        xc::color(14, 0);
+        cout << "KASIR V.1" << endl
+            << endl;
+
+        // NAMA KASIR
+        xc::color(7, 0);
+        cout << "KASIR: ";
+        xc::color(10, 0);
+        cout << "Adnan Muttaqin";
+
+        cout << endl;
+
+        // TABLE TOP
+        cout << char(218);
+        for (int i = 0; i < 68; i++)
         {
-            cout << char(194);
-            continue;
+            if (i == 6 || i == 44)
+            {
+                cout << char(194);
+                continue;
+            }
+            cout << char(196);
         }
-        cout << char(196);
-    }
-    cout << char(191);
+        cout << char(191);
 
-    cout << endl;
+        cout << endl;
 
-    // TABLE HEADER TOP
-    cout << char(179);
-    cout << "  NO  ";
-    cout << char(179);
-    cout << " MENU                                ";
-    cout << char(179);
-    cout << " HARGA                 ";
-    cout << char(179);
-
-    cout << endl;
-
-    // TABLE HEADER BOTTOM
-    cout << char(195);
-    for (int i = 0; i < 68; i++)
-    {
-        if (i == 6 || i == 44)
-        {
-            cout << char(197);
-            continue;
-        }
-        cout << char(196);
-    }
-    cout << char(180);
-
-    cout << endl;
-
-    // LOOP ARRAY
-    for (int i = 0; i < menuSize; i++)
-    {
-        int n = i + 1;
-        if (items[i].length() == 0)
-        {
-            break;
-        }
+        // TABLE HEADER TOP
         cout << char(179);
-        cout << "  " << n << "   ";
+        xc::color(7, 0);
+        cout << "  NO  ";
+        xc::color(10, 0);
         cout << char(179);
-
-        // STRING TO ARRAY: NAMA
-        int begin = 0;
-        int pos = items[i].find(',');
-        string nama = items[i].substr(begin, pos);
-        int sizeNama = 36 - nama.length();
-
-        // STRING T0 ARRAY: HARGA
-        begin = pos + 1;
-        pos = items[i].find(",", begin);
-        string harga = items[i].substr(begin, pos - begin);
-        int sizeharga = 19 - harga.length();
-
-        cout << " " << nama;
-        for (int s = 0; s < sizeNama; s++)
-        {
-            cout << " ";
-        }
+        xc::color(7, 0);
+        cout << " MENU                                ";
+        xc::color(10, 0);
         cout << char(179);
-
-        cout << " Rp." << harga;
-        for (int s = 0; s < sizeharga; s++)
-        {
-            cout << " ";
-        }
+        xc::color(7, 0);
+        cout << " HARGA                 ";
+        xc::color(10, 0);
         cout << char(179);
 
         cout << endl;
-    }
 
-    // TABLE BOTTOM
-    cout << char(192);
-    for (int i = 0; i < 68; i++)
-    {
-        if (i == 6 || i == 44)
+        // TABLE HEADER BOTTOM
+        cout << char(195);
+        for (int i = 0; i < 68; i++)
         {
-            cout << char(193);
-            continue;
+            if (i == 6 || i == 44)
+            {
+                cout << char(197);
+                continue;
+            }
+            cout << char(196);
         }
-        cout << char(196);
-    }
-    cout << char(217);
+        cout << char(180);
 
-    cout << endl
-         << endl;
+        cout << endl;
 
-    int idNow = 0;
-    int menuDipih[100];
-    int jumlahMenu[100];
-    for (;;)
-    {
-        string yN;
+        // LOOP ARRAY
+        for (int i = 0; i < menuSize; i++)
+        {
+            // UNTUK CHECK ARRAY YANG MEMPUNYAI NILAI
+            int n = i + 1;
+            if (items[i].length() == 0)
+            {
+                break;
+            }
+            cout << char(179);
+            xc::color(7, 0);
+            cout << "  " << n << "   ";
+            xc::color(10, 0);
+            cout << char(179);
 
-        cout << "Pilih Menu: ";
-        cin >> menuDipih[idNow];
+            // STRING TO ARRAY: NAMA
+            int begin = 0;
+            int pos = items[i].find(',');
+            string nama = items[i].substr(begin, pos);
+            int sizeNama = 36 - nama.length();
 
-        cout << "Jumlah Menu: ";
-        cin >> jumlahMenu[idNow];
+            // STRING T0 ARRAY: HARGA
+            begin = pos + 1;
+            pos = items[i].find(",", begin);
+            string harga = items[i].substr(begin, pos - begin);
+            int sizeharga = 19 - harga.length();
 
-        cout << "Apakah Anda Akan Memesan Lagi? (Y/n): ";
-        cin >> yN;
+            xc::color(7, 0);
+            cout << " " << nama;
+            for (int s = 0; s < sizeNama; s++)
+            {
+                cout << " ";
+            }
+            xc::color(10, 0);
+            cout << char(179);
 
-        idNow += 1;
+            xc::color(7, 0);
+            cout << " Rp." << harga;
+            for (int s = 0; s < sizeharga; s++)
+            {
+                cout << " ";
+            }
+            xc::color(10, 0);
+            cout << char(179);
 
-        if(yN == "n" || yN == "N") {
-            break;
+            cout << endl;
         }
 
-    }
+        // TABLE BOTTOM
+        cout << char(192);
+        for (int i = 0; i < 68; i++)
+        {
+            if (i == 6 || i == 44)
+            {
+                cout << char(193);
+                continue;
+            }
+            cout << char(196);
+        }
+        cout << char(217);
 
-    system("cls");
+        cout << endl
+            << endl;
 
-    // PRINT TABLE 2
-    cout << char(218);
-    for(int i = 0; i < 68; i++) {
-        cout << char(196);
-    }
-    cout << char(191);
+        int idNow = 0;
+        int menuDipih[100];
+        int jumlahMenu[100];
+        for (;;)
+        {
+            string yN;
+            xc::color(10, 0);
+            cout << "Pilih Menu: ";
+            xc::color(7, 0);
+            cin >> menuDipih[idNow];
 
-    cout << endl;
+            xc::color(10, 0);
+            cout << "Jumlah Menu: ";
+            xc::color(7, 0);
+            cin >> jumlahMenu[idNow];
 
-    // PESANAN ANDA
-    cout << char(179);
-    cout << "                           Pesanan Anda                             " << char(179);
-    cout << endl;
+            xc::color(10, 0);
+            cout << "Apakah Anda Akan Memesan Lagi? (Y/n): ";
+            xc::color(7, 0);
+            cin >> yN;
 
-    int totalHarga = 0;
-    // PESANAN ANDA VALUE
-    for(int v = 0; v < idNow; v++) {
-        int getArrayId = menuDipih[v] - 1;
-        int no = v + 1;
+            idNow += 1;
 
-        // STRING TO ARRAY: NAMA
-        int begin = 0;
-        int pos = items[getArrayId].find(',');
-        string nama = items[getArrayId].substr(begin, pos);
-        int sizeNama = 36 - nama.length();
+            if(yN == "n" || yN == "N") {
+                break;
+            }
 
-        // STRING T0 ARRAY: HARGA
-        begin = pos + 1;
-        pos = items[getArrayId].find(",", begin);
-        string harga = items[getArrayId].substr(begin, pos - begin);
-        int sizeharga = 19 - harga.length();
+        }
 
+        system("cls");
+
+        // PRINT TABLE 2
+        cout << char(218);
+        for(int i = 0; i < 68; i++) {
+            cout << char(196);
+        }
+        cout << char(191);
+
+        cout << endl;
+
+        // PESANAN ANDA
+        xc::color(10, 0);
         cout << char(179);
-
-        int hargaKali = stoi(harga) * jumlahMenu[v];
-
-        cout << " " << no << ".";
-        cout << "  " << nama;
-        cout << "  " << jumlahMenu[v];
-        cout << "  " << hargaKali;
-
-        string allString = "       ." + to_string(no)  + nama + to_string(jumlahMenu[v]) + to_string(hargaKali);
-        int countString = 68 - allString.length();
-
-        for (int s = 0; s < countString; s++)
-        {
-            cout << " ";
-        }
-
-
+        xc::color(7, 0);
+        cout << "                           Pesanan Anda                             ";
+        xc::color(10, 0);
         cout << char(179);
         cout << endl;
+
+        int totalHarga = 0;
+        // PESANAN ANDA VALUE
+        for(int v = 0; v < idNow; v++) {
+            int getArrayId = menuDipih[v] - 1;
+            int no = v + 1;
+
+            // STRING TO ARRAY: NAMA
+            int begin = 0;
+            int pos = items[getArrayId].find(',');
+            string nama = items[getArrayId].substr(begin, pos);
+            int sizeNama = 36 - nama.length();
+
+            // STRING T0 ARRAY: HARGA
+            begin = pos + 1;
+            pos = items[getArrayId].find(",", begin);
+            string harga = items[getArrayId].substr(begin, pos - begin);
+            int sizeharga = 19 - harga.length();
+
+            xc::color(10, 0);
+            cout << char(179);
+
+            int hargaKali = stoi(harga) * jumlahMenu[v];
         
-        totalHarga += hargaKali;
+            xc::color(7, 0);
+            cout << " " << no << ".";
+            cout << "  " << nama;
+            cout << "  " << jumlahMenu[v];
+            cout << "  " << hargaKali;
+
+            string allString = "       ." + to_string(no)  + nama + to_string(jumlahMenu[v]) + to_string(hargaKali);
+            int countString = 68 - allString.length();
+
+            for (int s = 0; s < countString; s++)
+            {
+                cout << " ";
+            }
+
+            xc::color(10, 0);
+            cout << char(179);
+            cout << endl;
+            
+            totalHarga += hargaKali;
+        }
+        
+        xc::color(10, 0);
+        cout << char(192);
+        for(int i = 0; i < 68; i++) {
+            cout << char(196);
+        }
+        cout << char(217);
+
+        cout << endl;
+
+        int uangAnda;
+        cout << "Total: " << totalHarga << endl;
+        cout << "Uang Anda: ";
+        xc::color(7, 0);
+        cin >> uangAnda;
+
+        system("cls");
+        xc::color(7, 0);
+        cout << endl << endl;
+        cout << "Kembalian Anda: " << (uangAnda - totalHarga);
+        cout << endl << endl;
+        cout << "Terima Kasih!!!";
+        cout << endl << endl << endl;
+    } else {
+        cout << "Maaf Aplikasi Tidak Bisa Dibuka, Karena Password Salah!";
     }
-    
-    cout << char(192);
-    for(int i = 0; i < 68; i++) {
-        cout << char(196);
-    }
-    cout << char(217);
-
-    cout << endl;
-
-    int uangAnda;
-    cout << "Total: " << totalHarga << endl;
-    cout << "Uang Anda: ";
-    cin >> uangAnda;
-
-    system("cls");
-    cout << endl << endl;
-    cout << "Kembalian Anda: " << (uangAnda - totalHarga);
-    cout << endl << endl;
-    cout << "Terima Kasih!!!";
-    cout << endl << endl << endl;
 
     return 0;
 }
